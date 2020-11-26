0f7d330c2a27422377b9329e2f95420fa621aa5d
r0ar-69 <shun199969kiyo@gmail.com>
2020-11-26 14:05:43 +0900

    const post & detail page

---
+++ yoru/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app :style="{ background: $vuetify.theme.themes.light.background }">
+    <v-container >
+      <img src="~/assets/main_logo.png" width="100%" class="px-16">
+      <v-row justify="center">
+        <h2>Sign in</h2>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+
+
+h2{
+  font-family: 'roboto';
+  font-size: 25px;
+}
+</style>
